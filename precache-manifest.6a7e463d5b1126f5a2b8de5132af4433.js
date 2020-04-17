ad6ad8f0a445dd7e03a55f550b569a9a4977d0da
Matthew Alvarado-Ross <maalvaradoross@wpi.edu>
2020-04-16 23:57:26 -0400

    Updates

---
+++ precache-manifest.6a7e463d5b1126f5a2b8de5132af4433.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c22d0709c0b24b86d17e9a63ca45f21",
+    "url": "/Bracket-Game-App/index.html"
+  },
+  {
+    "revision": "e64c1c6fccb61490bbfd",
+    "url": "/Bracket-Game-App/static/js/2.7169bb04.chunk.js"
+  },
+  {
+    "revision": "f29bccc07de57e2474a4eef485e4d41e",
+    "url": "/Bracket-Game-App/static/js/2.7169bb04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37ecb5731d151fabea63",
+    "url": "/Bracket-Game-App/static/js/main.a8091ab5.chunk.js"
+  },
+  {
+    "revision": "5adfbdbc362a70058feb",
+    "url": "/Bracket-Game-App/static/js/runtime-main.2ecf1ea4.js"
+  }
+]);
